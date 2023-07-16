7f51f86120882e2c0c9fb31a6f745978ef51d4aa
Vladimir Metelev <metelev_vladimir@mail.ru>
2023-07-16 13:44:02 +0300

    правки

--- controllers/user.js
+++ controllers/user.js
@@ -40,7 +40,7 @@ module.exports.createUser = (req, res) => {
 module.exports.updateProfile = (req, res) => {
   const {name, about} = req.body;
   const { _id } = req.user;
-  User.findByIdAndUpdate(_id, {name, about})
+  User.findByIdAndUpdate(_id, {name, about}, {new: true})
   .then((user) => res.send({data: user}))
   .catch((err) => {
     if(err.name === 'ValidationError'){
@@ -54,7 +54,7 @@ module.exports.updateProfile = (req, res) => {
 module.exports.updateAvatar = (req, res) => {
   const {avatar} = req.body;
   const { _id } = req.user;
-  User.findByIdAndUpdate(_id, {avatar})
+  User.findByIdAndUpdate(_id, {avatar}, {new: true})
   .then((user) => res.send({data: user}))
   .catch((err) => {
     if(err.name === 'ValidationError'){
