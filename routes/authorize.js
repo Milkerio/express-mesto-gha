76b3cbabcb46117a515bd905dab7c0cd999db5cb
Vladimir Metelev <metelev_vladimir@mail.ru>
2023-07-24 16:43:19 +0300

    project done

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,6 +1,7 @@
 const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const { login, createUser } = require('../controllers/user');
+const { validateUrl } = require('../validation/urlValidation');
 
 router.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -12,7 +13,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().custom(validateUrl),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
