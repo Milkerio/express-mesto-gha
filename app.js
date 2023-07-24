a7148aa9c2d42223e975621537995e12dd0193e0
Vladimir Metelev <metelev_vladimir@mail.ru>
2023-07-24 16:02:49 +0300

    project done

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-const { createUser, login } = require('./controllers/user');
+const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
@@ -11,24 +10,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(require('./routes/authorize'));
+
 app.use('/', auth, require('./routes/user'));
 app.use('/', auth, require('./routes/card'));
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Данного пути не существует.' });
 });
---
+++ routes/authorize.js
@@ -0,0 +1,21 @@
+const { celebrate, Joi } = require('celebrate');
+const router = require('express').Router();
+const { login, createUser } = require('../controllers/user');
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+module.exports = router;
